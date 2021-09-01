3ae50d3ed96f0a802b3325513e4326ff22fae97f
roy-rosario88 <89855462+roy-rosario88@users.noreply.github.com>
2021-09-01 12:09:45 -0400

    initialized files

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.hamburger');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
